0f9a893c6f90e8de59b5f790dab49cb69ba24292
Jimmy Zheng <jimmy_842@berkeley.edu>
2020-11-28 09:59:25 -0800

    Initial commit

---
+++ schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE todolists(
+  id serial PRIMARY KEY,
+  title text NOT NULL UNIQUE
+  username text NOT NULL
+);
+
+CREATE TABLE todos(
+  id serial PRIMARY KEY,
+  title text NOT NULL,
+  done boolean NOT NULL DEFAULT false,
+  username text NOT NULL,
+  todolist_id integer NOT NULL REFERENCES todolists(id) ON DELETE CASCADE
+);
